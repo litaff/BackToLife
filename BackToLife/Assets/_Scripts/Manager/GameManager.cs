811f7e6e3df887809799c8918fb11ebb93c46489
litaff <a.gazda2k18@gmail.com>
2022-07-12 22:25:29 +0200

    Removed next level concept

--- BackToLife/Assets/_Scripts/Manager/GameManager.cs
+++ BackToLife/Assets/_Scripts/Manager/GameManager.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
 
-// TODO: move everything related to buttons/scene management to SceneManager.cs
+// TODO: look further into improving this \/
 
 namespace BackToLife
 {
@@ -29,7 +29,7 @@ namespace BackToLife
 
         private void Start()
         {
-            _sceneManager.LoadScene(SceneManager.SceneType.Title).Invoke();
+            _sceneManager.LoadScene(SceneManager.SceneType.Menu).Invoke();
         }
 
         public void StartLevel()
@@ -39,12 +39,6 @@ namespace BackToLife
             _sceneManager.LoadScene(SceneManager.SceneType.Gameplay).Invoke();
         }
 
-        public void GetNextLevel()
-        {
-            _gridManager.enabled = true;
-            _gridManager.InitializeGrid(_levelManager.GetNextLevel());
-        }
-
         public void ResetLevel()
         {
             _gridManager.enabled = false;
@@ -52,12 +46,17 @@ namespace BackToLife
             _gridManager.InitializeGrid(_levelManager.ResetLevel());
         }
 
+        public void EndAll()
+        {
+            _gridManager.enabled = false;
+        }
+
         private void Update()
         {
             if (!_gridManager.enabled) return;
             _gridManager.GameUpdate(_inputManager.GetSwipeDirection(),gridUpdateSpeed);
             if (!_gridManager.WinCondition()) return;
-            _sceneManager.LoadScene(SceneManager.SceneType.Progress).Invoke();
+            _sceneManager.LoadScene(SceneManager.SceneType.EndLevel).Invoke();
             _gridManager.enabled = false;
         }
     }
--- BackToLife/Assets/_Scripts/Manager/LevelManager.cs
+++ BackToLife/Assets/_Scripts/Manager/LevelManager.cs
@@ -8,18 +8,11 @@ namespace BackToLife
     {
         [SerializeField]private List<GridPattern> gridPatterns;
 
-        private UIManager _uiManager;
         private GridPattern _currentPattern;
 
         private void Awake()
         {
-            _uiManager = gameObject.transform.parent.GetComponentInChildren<UIManager>();
             _currentPattern = GetNextPattern();
-        }
-
-        private bool MorePatterns()
-        {
-            return gridPatterns.Count > 0;
         }
 
         private GridPattern GetNextPattern()
@@ -39,21 +32,6 @@ namespace BackToLife
             Debug.LogError($"{_currentPattern.name} is not valid!");
             return null;
 
-        }
-
-        public GridPattern GetNextLevel()
-        {
-            _currentPattern = null;
-            _uiManager.SetPageActive(Page.PageType.Win, false);
-            if (!MorePatterns())
-            {
-                _uiManager.SetPageActive(Page.PageType.NoFun, true);
-                _uiManager.SetPageActive(Page.PageType.Gameplay, false);
-                return null;
-            }
-            _uiManager.SetPageActive(Page.PageType.Gameplay, true);
-            _currentPattern = GetNextPattern();
-            return _currentPattern;
         }
 
         public GridPattern ResetLevel()
