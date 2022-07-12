4fedb01443e2acf7aee7f3a9b1036eb1b9b0f292
litaff <a.gazda2k18@gmail.com>
2022-07-12 21:38:06 +0200

    Added Helper static class
    Moved ReverseString to here
    Should contain generic functions like ReverseString

---
+++ BackToLife/Assets/_Scripts/Helper.cs
@@ -0,0 +1,14 @@
+﻿namespace BackToLife
+{
+    public static class Helper
+    {
+        public static string ReverseString(string str) {  
+            var chars = str.ToCharArray();  
+            var result = new char[chars.Length];  
+            for (int i = 0, j = str.Length - 1; i < str.Length; i++, j--) {  
+                result[i] = chars[j];  
+            }  
+            return new string(result);  
+        }
+    }
+}
