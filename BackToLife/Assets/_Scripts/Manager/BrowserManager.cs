939da1e7ce8c3c30097ed583aebc5b231c3cbd35
litaff <a.gazda2k18@gmail.com>
2022-08-02 15:39:09 +0200

    Update BrowserManager.cs
    TODO specification

--- BackToLife/Assets/_Scripts/Manager/BrowserManager.cs
+++ BackToLife/Assets/_Scripts/Manager/BrowserManager.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-// TODO: Implement browser, should be a scrollable list of levels, sortable by rating, date of creation, and a mix of those two
+// TODO: Implement browser, should be a scrollable list of levels, sortable by rating, date of creation, and a mix of those two (trending-new high rated)
 // TODO: Use browser for tutorial handling, there should be a couple of level to introduce each block and tile
 
 namespace BackToLife
